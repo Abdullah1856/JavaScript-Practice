b6cf2c3d8a6925b42d039244450f4c65638de913
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2023-04-16 22:43:03 -0700

    Add files via upload

---
+++ JavaScript First Learning Code/script.js
@@ -0,0 +1,83 @@
+/* let js = 'Abdullah';
+console.log(2 + 2 + 2 + 2);
+
+let death = "Abdullah is dead";
+console.log(death);
+
+let myFisrtJob = "Front-End Developer";
+let mySecondJob = "Interni React Native";
+
+let myCountryPopulation = "20moiilion";
+let myCityPopulation = "1milion";
+console.log(myCityPopulation, myCountryPopulation)
+
+true;
+console.log(typeof true);
+
+let abdullahIsDeath = 'Yes';
+console.log(typeof abdullahIsDeath);
+
+let ageOfAbdullah = 19;
+console.log(typeof ageOfAbdullah)
+
+echo = My fisrt jo is 'myFisrtJob' and second job is 'mySecondJob' 
+
+
+let abdullah = true;
+console.log(typeof abdullah);
+
+abdullah = "Dead";
+console.log(typeof abdullah);
+
+abdullah = 23 + 332;
+console.log(typeof abdullah);
+
+
+const ageAbdullah = 2023 - 2003;
+const ageBeautyOnStreet = 2023 - 2000;
+console.log(ageAbdullah, ageBeautyOnStreet);
+
+let x = 10 + 5;
+console.log(x);
+x += 50;
+console.log(x);
+
+
+
+const heightMark = (1.69);
+const weightMark = (78);
+
+const heightJohn = (1.76);
+const weightJohn = (92);
+
+*/
+
+
+// Math operators
+const now = 2023
+const ageAbdullah = now - 2003;
+const ageAnamta = now - 2002;
+console.log(ageAbdullah, ageAnamta);
+console.log(ageAbdullah * 2 / 9, ageAnamta * 10 / 898, 6 ** 8);
+
+
+// Assignment operators
+const son = ("Abdullah");
+const father = ("Arshad");
+console.log(son + ' ' + father);
+let x = (20 + 30);
+x += 20;
+x *= 2;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageAnamta > ageAbdullah);
+console.log(ageAnamta >= 20);
+
+const trueAge = (ageAnamta >= 20);
+console.log(trueAge);
+
+console.log(now - 2002 > now - 2003);
